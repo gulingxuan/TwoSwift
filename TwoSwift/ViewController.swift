3496fa74eae313f321cf15d91ded98aee6c3ae8d
ChenDan <chend@hgxcloud.com>
2017-05-19 14:10:49 +0800

    Initial Commit

---
+++ TwoSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwoSwift
+//
+//  Created by 顾泠轩 on 2017/5/19.
+//  Copyright © 2017年 glx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
